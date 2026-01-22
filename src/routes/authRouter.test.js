2e98d9ae6a270dd15c868612579b0282771c6423
Micaela Madariaga <madariagamica20@gmail.com>
2026-01-22 16:18:49 -0700

    Added coverage updating to the ci workflow

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 const app = require("../service");
-const { Role, DB } = require("../database/database.js");
+// const { Role, DB } = require("../database/database.js");
 
 const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
 let testUserAuthToken;
@@ -28,15 +28,15 @@ function expectValidJwt(potentialJwt) {
   );
 }
 
-function randomName() {
-  return Math.random().toString(36).substring(2, 12);
-}
+// function randomName() {
+//   return Math.random().toString(36).substring(2, 12);
+// }
 
-async function createAdminUser() {
-  let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
-  user.name = randomName();
-  user.email = user.name + "@admin.com";
+// async function createAdminUser() {
+//   let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
+//   user.name = randomName();
+//   user.email = user.name + "@admin.com";
 
-  user = await DB.addUser(user);
-  return { ...user, password: "toomanysecrets" };
-}
+//   user = await DB.addUser(user);
+//   return { ...user, password: "toomanysecrets" };
+// }
