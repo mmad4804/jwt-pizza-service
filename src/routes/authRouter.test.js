b6108b34009ddfbbbb764766a6e46aec7c45c139
Micaela Madariaga <madariagamica20@gmail.com>
2026-01-27 19:45:01 -0700

    added user router tests

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -4,12 +4,14 @@ const app = require("../service");
 
 const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
 let testUserAuthToken;
+let userId;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
   const registerRes = await request(app).post("/api/auth").send(testUser);
   testUserAuthToken = registerRes.body.token;
   expectValidJwt(testUserAuthToken);
+  userId = registerRes.body.user.id;
 });
 
 test("login", async () => {
@@ -22,8 +24,30 @@ test("login", async () => {
   expect(loginRes.body.user).toMatchObject(expectedUser);
 });
 
+test("update user", async () => {
+  const userUpdate = {
+    name: randomName(),
+    email: randomName() + "@test.com",
+    password: "b",
+  };
+  const updateUserRes = await request(app)
+    .put(`/api/user/${userId}`)
+    .set("Authorization", `Bearer ${testUserAuthToken}`)
+    .send(userUpdate);
+
+  expect(updateUserRes.status).toBe(200);
+  expectValidJwt(updateUserRes.body.token);
+  expect(updateUserRes.body.user.name).toBe(userUpdate.name);
+  expect(updateUserRes.body.user.email).toBe(userUpdate.email);
+  expect(updateUserRes.body.user.id).toBe(userId);
+});
+
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(
     /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/,
   );
+}
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
 }
