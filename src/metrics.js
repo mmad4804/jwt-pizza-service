b45171f76e40c641bdd46f31c4b9438a25547cbc
Micaela Madariaga <madariagamica20@gmail.com>
2026-03-26 18:50:10 -0600

    Added functionality for pizza tracking

--- src/metrics.js
+++ src/metrics.js
@@ -88,6 +88,23 @@ setInterval(() => {
     createMetric("memory_usage_percent", memRes, "%", "gauge", "asDouble", {}),
   );
 
+  metrics.push(
+    createMetric("pizzas_sold_total", totalPizzasSold, "1", "sum", "asInt", {}),
+  );
+  metrics.push(
+    createMetric(
+      "creation_failures_total",
+      totalCreationFailures,
+      "1",
+      "sum",
+      "asInt",
+      {},
+    ),
+  );
+  metrics.push(
+    createMetric("revenue_total", totalRevenue, "USD", "sum", "asDouble", {}),
+  );
+
   if (metrics.length > 0) sendMetricToGrafana(metrics);
 }, 5000); // Send every 5 seconds (adjust as needed)
 
@@ -109,6 +126,19 @@ function activeUserTracker(req, res, next) {
 function incrementAuthAttempt(outcome) {
   const key = `auth_${outcome}`;
   requests[key] = (requests[key] || 0) + 1;
+}
+
+let totalPizzasSold = 0;
+let totalCreationFailures = 0;
+let totalRevenue = 0;
+
+function incrementPizzaMetrics(pizzaCount, revenue, isFailure) {
+  if (isFailure) {
+    totalCreationFailures++;
+  } else {
+    totalPizzasSold += pizzaCount;
+    totalRevenue += revenue;
+  }
 }
 
 function createMetric(
@@ -195,77 +225,5 @@ module.exports = {
   createMetric,
   sendMetricToGrafana,
   incrementAuthAttempt,
+  incrementPizzaMetrics,
 };
-
-// let requests = 0;
-// let latency = 0;
-
-// setInterval(() => {
-//   const cpuValue = Math.floor(Math.random() * 100) + 1;
-//   sendMetricToGrafana("cpu", cpuValue, "gauge", "%");
-
-//   requests += Math.floor(Math.random() * 200) + 1;
-//   sendMetricToGrafana("requests", requests, "sum", "1");
-
-//   latency += Math.floor(Math.random() * 200) + 1;
-//   sendMetricToGrafana("latency", latency, "sum", "ms");
-// }, 1000);
-
-// function sendMetricToGrafana(metricName, metricValue, type, unit) {
-//   const metric = {
-//     resourceMetrics: [
-//       {
-//         scopeMetrics: [
-//           {
-//             metrics: [
-//               {
-//                 name: metricName,
-//                 unit: unit,
-//                 [type]: {
-//                   dataPoints: [
-//                     {
-//                       asInt: metricValue,
-//                       timeUnixNano: Date.now() * 1000000,
-//                     },
-//                   ],
-//                 },
-//               },
-//             ],
-//           },
-//         ],
-//       },
-//     ],
-//   };
-
-//   if (type === "sum") {
-//     metric.resourceMetrics[0].scopeMetrics[0].metrics[0][
-//       type
-//     ].aggregationTemporality = "AGGREGATION_TEMPORALITY_CUMULATIVE";
-//     metric.resourceMetrics[0].scopeMetrics[0].metrics[0][type].isMonotonic =
-//       true;
-//   }
-
-//   const body = JSON.stringify(metric);
-//   fetch(`${config.endpointUrl}`, {
-//     method: "POST",
-//     body: body,
-//     headers: {
-//       Authorization: `Bearer ${config.accountId}:${config.apiKey}`,
-//       "Content-Type": "application/json",
-//     },
-//   })
-//     .then((response) => {
-//       if (!response.ok) {
-//         response.text().then((text) => {
-//           console.error(
-//             `Failed to push metrics data to Grafana: ${text}\n${body}`,
-//           );
-//         });
-//       } else {
-//         console.log(`Pushed ${metricName}`);
-//       }
-//     })
-//     .catch((error) => {
-//       console.error("Error pushing metrics:", error);
-//     });
-// }
