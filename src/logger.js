32591c47f9f48e7620886fe6fdb3fdf0299b5b97
Micaela Madariaga <madariagamica20@gmail.com>
2026-03-30 15:45:28 -0600

    Removed redundant logic

--- src/logger.js
+++ src/logger.js
@@ -23,47 +23,7 @@ class Logger {
   }
 
   log(level, type, logData) {
-    const labels = {
-      component: config.logging.source,
-      level: level,
-      type: type,
-    };
-
-    this.factory.log(labels, this.sanitize(logData));
-  }
-
-  statusToLogLevel(statusCode) {
-    if (statusCode >= 500) return "error";
-    if (statusCode >= 400) return "warn";
-    return "info";
-  }
-
-  nowString() {
-    return (Math.floor(Date.now()) * 1000000).toString();
-  }
-
-  sanitize(logData) {
-    let logString = JSON.stringify(logData);
-    // Sanitize passwords, tokens, and credit cards
-    return logString
-      .replace(/\\"password\\":\s*\\"[^"]*\\"/g, '\\"password\\":\\"*****\\"')
-      .replace(/\\"token\\":\s*\\"[^"]*\\"/g, '\\"token\\":\\"*****\\"')
-      .replace(/\\"email\\":\s*\\"[^"]*\\"/g, '\\"email\\":\\"*****\\"')
-      .replace(/\\"jwt\\":\s*\\"[^"]*\\"/g, '\\"jwt\\":\\"*****\\"');
-  }
-
-  sendLogToGrafana(event) {
-    const body = JSON.stringify(event);
-    fetch(`${config.logging.endpointUrl}`, {
-      method: "post",
-      body: body,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${config.logging.accountId}:${config.logging.apiKey}`,
-      },
-    }).then((res) => {
-      if (!res.ok) console.log("Failed to send log to Grafana");
-    });
+    this.factory.log(level, type, logData);
   }
 }
 module.exports = new Logger();
