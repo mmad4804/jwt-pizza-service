2fe05dcdf3f2d1f41e4eb81fc2209ec5ee23b1e3
Micaela Madariaga <madariagamica20@gmail.com>
2026-03-30 22:38:34 -0600

    Added logging for unhandled exceptions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 const app = require("./service.js");
+const logger = require("./logger.js");
+
+process.on("unhandledRejection", (reason, promise) => {
+  logger.log("error", "unhandledRejection", {
+    reason: reason.message || reason,
+    stack: reason.stack,
+  });
+});
+
+// Catch uncaught exceptions
+process.on("uncaughtException", (err) => {
+  logger.log("error", "uncaughtException", {
+    message: err.message,
+    stack: err.stack,
+  });
+  process.exit(1);
+});
 
 const port = process.argv[2] || 5000;
 app.listen(port, () => {
